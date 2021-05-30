e494fa28f060b29074e0817504a4b5cc52a6fa93
Kacper Ratyński <kacperratynski1@gmail.com>
2021-05-30 13:33:06 +0200

    scrollbar positioned

--- src/components/IssuesWindow/IssuesWindow.jsx
+++ src/components/IssuesWindow/IssuesWindow.jsx
@@ -5,15 +5,17 @@ import styles from './IssuesWindow.module.css';
 
 export default function IssuesWindow() {
   return (
-    <div className={styles.issuesWindowWrapper}>
-      {data.map((day) => (
-        <ul key={day.id}>
-          <div className={styles.issueDate}>{day.date}</div>
-          {day.issues.map((issue) => (
-            <Issue key={issue.id} issue={issue.issue} />
-          ))}
-        </ul>
-      ))}
+    <div className={styles.wrapper}>
+      <div className={styles.issuesWindowWrapper}>
+        {data.map((day) => (
+          <ul key={day.id}>
+            <div className={styles.issueDate}>{day.date}</div>
+            {day.issues.map((issue) => (
+              <Issue key={issue.id} issue={issue.issue} />
+            ))}
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
